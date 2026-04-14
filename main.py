54c53d75466e684d12ca6db46c7bf73895cba6ca
Isabel <43370060+isabelge@users.noreply.github.com>
2026-04-14 06:27:45 +0200

    Remove subtraction function from calculator
    Removed the subtraktion function and related calls.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 def addieren(a, b):
     return a + b
 
-def subtraktion(a, b):
-    return a - b
-
 def main():
     print("Taschenrechner")
     print("1 fuer Addition")
@@ -18,13 +15,10 @@ def main():
             print("Resultat: {}".format(addieren(2,3)))
             # Hier Funktion für Addition einfügen
         case "2":
-            print("Resultat: {}".format(subtraktion(3, 1)))
             # Hier Funktion für die Subtraktion einfügen
         case "3":
-            print("Gruppenmitglied 2 kann hier die Multiplikation implementieren.")
             # Hier die Funktion für die Multiplikation einfügen
         case "4":
-            print("Gruppenmitglied 2 kann hier die Division implementieren.")
             # Hier die Funktion für die Division einfügen
         case _:
             print("Falsche Auswahl, das Programm wird beendet.")
